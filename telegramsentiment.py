85c0706c050022d787c3ec5ebd411f67c7dad9a8
davidlwl <davidlwl3@gmail.com>
2017-05-17 00:08:19 +0800

    Update telegramsentiment.py

--- telegramsentiment.py
+++ telegramsentiment.py
@@ -1,49 +1,11 @@
-import sys
-from collections import Counter
-import re
-import operator
-import pickle
-'''
-non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd) #change emoticons
-       
-count_all = Counter()
-t_only = pickle.load( open( "d.p", "rb" ))
-d_only = pickle.load( open( "d.p", "rb" ) )
-g_only = pickle.load( open( "g.p", "rb" ) )
-count_all.update(terms_only)
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+    #note: depending on how you installed (e.g., using source code download versus pip install), you may need to import like this:
+    #from vaderSentiment import SentimentIntensityAnalyzer
 
-print(count_all.most_common(30))
+# --- examples -------
 
-terms_only = [term for term in preprocess(" ".join(d))\
-                      if term not in stop and\
-                      not term.startswith(('#', '@'))]
-d_only = [term for term in preprocess(" ".join(d))\
-                      if term not in stop and\
-                      not term.startswith(('#', '@'))]
-g_only = [term for term in preprocess(" ".join(g))\
-                      if term not in stop and\
-                      not term.startswith(('#', '@'))]'''
-
-from textblob import TextBlob
-def get_tweet_sentiment(tweet):
-    '''
-    Utility function to classify sentiment of passed tweet
-    using textblob's sentiment method
-    '''
-    # create TextBlob object of passed tweet text
-    analysis = TextBlob(tweet)
-    # set sentiment
-    if analysis.sentiment.polarity > 0:
-        return 'positive'
-    elif analysis.sentiment.polarity == 0:
-        return 'neutral'
-    else:
-        return 'negative'
-    
 while True:
-    tweet = input('please input phrase/word \n>')
-    print(get_tweet_sentiment(tweet))
-    print('-'*60)
-    
-    
-        
+    sentences = input('please input your sentence! \n>')
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(sentences)
+    print("{:-<65} {}".format(sentences, str(vs)))
