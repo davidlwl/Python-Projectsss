4cb046c266d6f0b5fa9e951b8f95049ef9b0298c
davidlwl <davidlwl3@gmail.com>
2017-05-16 21:08:38 +0800

    show download 2

--- show download.py
+++ show download.py
@@ -5,10 +5,10 @@ import time
 #Getting search results
 
 asking = int(input('Do you need suggestions? Yes - 1, No - 2\n>'))
-searcher = input('Enter movie/tv show name: ')
-def download_now(url):
-    browser = webdriver.Chrome(r'C:\Users\Davidlwl\Desktop\chromedriver.exe')
-    browser.get(url)
+
+def download_now():
+    
+    
     time.sleep(3)
     browser.find_element_by_id('player').click()
     time.sleep(5)
@@ -17,8 +17,7 @@ def download_now(url):
     
 
 
-def get_subtitles():
-    movie_id = searcher
+def get_subtitles(movie_id):
     urls ='https://www.opensubtitles.org/en/search2/sublanguageid-eng/moviename-' + movie_id
     print(urls)
     sc = requests.get(urls)
@@ -32,7 +31,7 @@ def get_subtitles():
         print (str(j+1)+'. '+anime[i].text.replace('\n',''))
         j += 1
     user_input = int(input('Enter from which subtitle to start download from: '))
-    browser = webdriver.Chrome(r'C:\Users\Davidlwl\Desktop\chromedriver.exe')
+    browser = webdriver.Chrome()
     browser.execute_script("window.open('www.google.com', 'new_window')")
     browser.switch_to.window(browser.window_handles[1])
     browser.get(amd[user_input-1])
@@ -53,7 +52,7 @@ def get_shows(url):
     print(dsa)
 
     shopList = [] 
-    maxLengthList = 5
+    maxLengthList = 10
     while len(shopList) < maxLengthList:
         users_input = int(input('Enter from which ep no. to start download \nEnter 0 to stop: '))
         shopList.append(users_input)
@@ -66,31 +65,28 @@ def get_shows(url):
 def search(d):
     mortv = int(input('Are you watching a tv show(1) or movie(2)?'))
     url = 'https://fmovies.is/search?keyword=' + d
-    sc = requests.get(url)
-    soup = BeautifulSoup(sc.text,'lxml')
-    anime = soup.select('.name')
-    j = 0
-    animes = []
-    for i in range(len(anime)):
-        f = anime[i].get('href')
-        animes.append('https://fmovies.is' + f)
-        print (str(j+1)+'. '+anime[i].text.replace('\n',''))
-        j += 1
-
-    #Selecting the movie to download
-    user_input = int(input('Enter the anime no. to download: '))
-    anime_url = animes[user_input-1]
-    sc = requests.get(anime_url)
-    soup = BeautifulSoup(sc.text,'lxml')
-    su = soup.select('.episodes a')
-    url = 'https://fmovies.is' + su[0].get('href')
+    browser = webdriver.Chrome()
+    browser.get(url)
+    browser.refresh()
+    moviename = browser.find_element_by_class_name('name')
+    moviename.click()
+    
+    time.sleep(5)
+    browser.find_element_by_id('player').click()
+    time.sleep(3)
+    elem = browser.find_element_by_xpath("(//div[@class='item mbtn download movie pull-right'])")
+    elem.click()
+    elems = browser.find_element_by_xpath("(//div[@class='item mbtn download subtitle pull-right hidden-xs'])") 
+    movie_id = d
+    
 
     if mortv == 2:
-        download_now(url)
-        get_subtitles()
+        get_subtitles(movie_id)
     elif mortv == 1:
         get_shows(url)
-        get_subtitles()                       
+        asker = input('are the downloads ready?')
+        if asker == 'yes':        
+            get_subtitles()                       
                          
 
 def suggestions():
@@ -112,14 +108,45 @@ def suggestions():
         print (str(i+1)+'. '+title[i].text)
         titles.append(title[i].text)
         print('-' * 60)
-        print(" ".join(link[i:i+2]))
+        print(" ".join(link[2*i:2*i+2]))
+        
 
     number = int(input('which movie would you want to watch?'))
     searcher = titles[number - 1]
+    print(searcher)
     search(searcher)
 
 if asking == 1:
     suggestions()
     
-
+searcher = input('Enter movie/tv show name: ')
 search(searcher)
+
+
+'''
+def search(d):
+    mortv = int(input('Are you watching a tv show(1) or movie(2)?'))
+    url = 'https://fmovies.is/search?keyword=' + d
+    sc = requests.get(url)
+    soup = BeautifulSoup(sc.text,'lxml')
+    anime = soup.select('.name')
+    j = 0
+    animes = []
+    for i in range(len(anime)):
+        f = anime[i].get('href')
+        animes.append('https://fmovies.is' + f)
+        print (str(j+1)+'. '+anime[i].text.replace('\n',''))
+        j += 1
+
+    #Selecting the movie to download
+    user_input = int(input('Enter the anime no. to download: '))
+    anime_url = animes[user_input-1]
+    sc = requests.get(anime_url)
+    soup = BeautifulSoup(sc.text,'lxml')
+    su = soup.select('.episodes a')
+    url = 'https://fmovies.is' + su[0].get('href')'''
+
+    
+
+    
+
